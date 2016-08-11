5e6100c4251be60d979f9696091040ac566810ff
aheddar <heddar.abdelilah@example.com>
2016-08-11 22:41:06 +0200

    First commit terraform.tf : work fine

---
+++ terraform.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "us-west-2"
+}
+#
+resource "aws_instance" "example" {
+  ami           = "ami-d732f0b7"
+  instance_type = "t2.micro"
+}

25b5c5f3b068cb83315b8689cd63a881cea5c57b
LinuxArchitects <heddar.abdelilah@live.fr>
2016-08-11 22:44:50 +0200

    Config repo on my git cli

--- terraform.tf
+++ terraform.tf
@@ -3,7 +3,7 @@ provider "aws" {
   secret_key = "${var.secret_key}"
   region     = "us-west-2"
 }
-
+#
 resource "aws_instance" "example" {
   ami           = "ami-d732f0b7"
   instance_type = "t2.micro"

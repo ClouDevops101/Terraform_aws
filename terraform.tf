f4214bb51ab5b5c1833f5be85fa9711abdb6a3e1
LinuxArchitects <heddar.abdelilah@live.fr>
2016-08-12 12:20:50 +0200

    Terraform_Aws module : adding security group

--- terraform.tf
+++ terraform.tf
@@ -3,9 +3,46 @@ provider "aws" {
   secret_key = "${var.secret_key}"
   region     = "us-west-2"
 }
-#
+# Creating instance
 resource "aws_instance" "example" {
   ami           = "ami-d732f0b7"
   instance_type = "t2.micro"
   key_name = "${var.key_name}"
+}
+# Security group ssh-in & http(s)-in  
+resource "aws_security_group" "Terraform-default" {
+    name = "terraform_example"
+    description = "Used in the terraform"
+
+    # SSH access from anywhere
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    # HTTP access from anywhere
+    ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    # HTTP access from anywhere
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    # outbound internet access
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 }

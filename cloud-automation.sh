c05eb57cfbdb02fde8e6759792691b86cc7f2909
LinuxArchitects <heddar.abdelilah@live.fr>
2016-08-13 15:30:30 +0200

    Cloud-automation : adding fashion warring message

--- cloud-automation.sh
+++ cloud-automation.sh
@@ -9,11 +9,9 @@
 # Verify parameter and trigger usage 
 usage(){
 	echo "Usage:" 
-        echo "     cloud-automation.sh    <app>       <environment> <num_servers> <server_size>"
-        echo "ex:  cloud-automation.sh hello_world          dev         2          t1.micro"
-        #echo "Recived args : $1 $2 $3 $4"
-        echo "warinig : default value will be applied, this may generate some additional cost "
-        echo "App : $App_Name Env : $Env Num_serv : $Num_Serv Serv_Size : $Serv_Size "
+        echo "      cloud-automation.sh    <app>       <environment> <num_servers> <server_size>"
+        echo "/!\   warinig : default value will be applied, this may generate some additional cost "
+        echo -e "      cloud-automation.sh \e[5m $App_Name $Env $Num_Serv $Serv_Size \e[0m"
         
 }
 #Default value
@@ -29,30 +27,42 @@ Num_Serv=${3:-$Num_Serv}
 # Arg 4
 Serv_Size="t2.micro"
 Serv_Size=${4:-$Serv_Size}
-# Prvent user from using an applying default value
-[[ $# -eq 0 ]] && usage
 
 # Advanced parameter verification
 # Number of server
-#re='^[0-9]+$'
-#if ! [[ $3 =~ $re ]] ; then
-#   echo -e "Error: Not a number\n" >&2; usage
-#fi
+re='^[0-9]+$'
+if ! [ -z "$3" ]
+  then
+      if ! [[ $3 =~ $re ]] ; then
+      echo -e "/!\ Num_Serv: Not a number"
+      echo -e "    Default value will be applied\n" >&2
+      Num_Serv="1"
+      usage
+      fi
+fi
 # Server Size
 re='^.*\..*+$'
-if ! [[ $4 =~ $re ]] ; then
-   echo -e "Error: Not a valid Server size\n Please refer to https://aws.amazon.com/fr/ec2/instance-types/\n" >&2; usage
+if ! [ -z "$4" ]
+  then
+      if ! [[ $4 =~ $re ]] ; then
+      echo -e "/!\ Not a valid Server size default value will be applied"
+      echo -e "Please refer to https://aws.amazon.com/fr/ec2/instance-types/\n" >&2
+      Serv_Size="t2.micro"
+      usage
+   fi
 fi
+# Prvent user from using an applying default value
+[[ $# -eq 0 ]] && usage
 #echo "$1 $2 $3 $4"
 echo "Starting Terraform"
 AWS_ELB_DNS=$(terraform apply  -var "app_name=$App_Name" -var "env_name=$Env" -var "num_serv=\"$Num_Serv\"" -var "serv_size=$Serv_Size" | grep address | awk -F= '{print $2}' | sed -e 's/\s//g' | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g" )
 echo "Terraform ended"
 # sleep waiting for amazon instance 70 second per instance
-#time_to_zzZZZ=$(( 70  * $3 ))
-#sleep $time_to_zzZZZ
+time_to_zzZZZ=$(( 70  * $3 ))
+sleep $time_to_zzZZZ
 
 # Starting Ansible"
-#ansible-playbook -i terraform.py -u ubuntu playbook.yml --private-key ~/.ssh/AWSNEWKEY.pem  >/dev/null  2>&1 
+ansible-playbook -i terraform.py -u ubuntu playbook.yml --private-key ~/.ssh/AWSNEWKEY.pem  >/dev/null  2>&1 
 # Wait for elb IP
 echo "waiting for elb"
  while true
